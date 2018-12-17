001d490c8b3d8fab5e0208b380befcc959f3407c
catallo david <=>
2018-12-17 14:34:11 +0100

    add date and add app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'blog';
+  posts: Array<Object> = [
+    {
+      title: "Mon premier post",
+      content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis molestias excepturi iure qui doloribus autem deserunt voluptates mollitia explicabo. Qui beatae illum nostrum consequuntur asperiores officia temporibus numquam alias ratione.",
+      loveIts: 0,
+      created_at: new Date()
+    },
+    {
+      title: "Mon deuxième message",
+      content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis molestias excepturi iure qui doloribus autem deserunt voluptates mollitia explicabo. Qui beatae illum nostrum consequuntur asperiores officia temporibus numquam alias ratione.",
+      loveIts: 0,
+      created_at: new Date()
+    },
+    {
+      title: "Enfin, un troisième article",
+      content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis molestias excepturi iure qui doloribus autem deserunt voluptates mollitia explicabo. Qui beatae illum nostrum consequuntur asperiores officia temporibus numquam alias ratione.",
+      loveIts: 0,
+      created_at: new Date()
+    }
+  ];
 }
--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post-list',
@@ -7,26 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostListComponent implements OnInit {
 
-  posts: Array<Object> = [
-    {
-      title: "Mon premier post",
-      content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis molestias excepturi iure qui doloribus autem deserunt voluptates mollitia explicabo. Qui beatae illum nostrum consequuntur asperiores officia temporibus numquam alias ratione.",
-      loveIts: 0,
-      created_at: new Date()
-    },
-    {
-      title: "Mon deuxième message",
-      content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis molestias excepturi iure qui doloribus autem deserunt voluptates mollitia explicabo. Qui beatae illum nostrum consequuntur asperiores officia temporibus numquam alias ratione.",
-      loveIts: 0,
-      created_at: new Date()
-    },
-    {
-      title: "Enfin, un troisième article",
-      content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis molestias excepturi iure qui doloribus autem deserunt voluptates mollitia explicabo. Qui beatae illum nostrum consequuntur asperiores officia temporibus numquam alias ratione.",
-      loveIts: 0,
-      created_at: new Date()
-    }    
-  ];
+  @Input() posts;
+  
   constructor() { }
 
   ngOnInit() {
