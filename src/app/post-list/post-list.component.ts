d09bb987aa2a45ab55faecc1142fc0089534a427
catallo david <=>
2018-12-17 12:30:47 +0100

    passing data from post-list to post component

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -7,6 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostListComponent implements OnInit {
 
+  posts: Array<Object> = [
+    {
+      title: "Mon premier post",
+      content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis molestias excepturi iure qui doloribus autem deserunt voluptates mollitia explicabo. Qui beatae illum nostrum consequuntur asperiores officia temporibus numquam alias ratione.",
+      LoveIts: 0,
+      created_at: new Date()
+    },
+    {
+      title: "Mon deuxième message",
+      content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis molestias excepturi iure qui doloribus autem deserunt voluptates mollitia explicabo. Qui beatae illum nostrum consequuntur asperiores officia temporibus numquam alias ratione.",
+      LoveIts: 0,
+      created_at: new Date()
+    },
+    {
+      title: "Enfin, un troisième article",
+      content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis molestias excepturi iure qui doloribus autem deserunt voluptates mollitia explicabo. Qui beatae illum nostrum consequuntur asperiores officia temporibus numquam alias ratione.",
+      LoveIts: 0,
+      created_at: new Date()
+    }    
+  ];
   constructor() { }
 
   ngOnInit() {
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { post } from 'selenium-webdriver/http';
 
 @Component({
   selector: 'app-post',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
+  @Input() post: Object;
   constructor() { }
 
   ngOnInit() {
